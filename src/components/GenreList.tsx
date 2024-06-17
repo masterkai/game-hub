0c067760c7b769576ec27840b3f371954ac960e4
maxLiu <neoliukai@gmail.com>
2024-06-17 17:56:00 +0800

    feat: add genre sidebar (main)
    Adds a sidebar to the main page that displays a list of genres. This allows users to filter games by genre, making it easier to find games they're interested in.

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import useGenre from "../hooks/useGenre";
+interface Props {
+  // Define your component's props here
+}
+
+const GenreList: React.FC<Props> = () => {
+  // Implement your component logic here
+  const { genres, error, isLoading } = useGenre();
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
---
+++ src/hooks/useGenre.ts
@@ -0,0 +1,44 @@
+import { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresProps {
+  count: number;
+  results: Genre[];
+}
+
+const useGenre = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [error, setError] = useState("");
+  // use bool loadind state
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    apiClient
+      .get<FetchGenresProps>("/genres", { signal: controller.signal })
+      .then((response) => {
+        setGenres(response.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+  return {
+    genres,
+    error,
+    // return loading state
+    isLoading,
+  };
+};
+export default useGenre;
