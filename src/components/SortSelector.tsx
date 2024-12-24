7d4d01ead280d9ac99eda559a84cbeb286fc3599
maxLiu <neoliukai@gmail.com>
2024-12-24 21:32:58 +0800

    chore(deps): update axios to v1.7.4
    Update axios dependency to version 1.7.4 This also
    updates follow-redirects to version 1.15.9

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+interface Props {
+  onSelectSortOrder: (order: string) => void;
+}
+
+const SortSelector = ({ onSelectSortOrder }: Props) => {
+  const sortOrders = [
+    { value: "Relevance", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "Name", label: "Name" },
+    { value: "-Released date", label: "Released date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order By Relevance
+      </MenuButton>
+      <MenuList>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
